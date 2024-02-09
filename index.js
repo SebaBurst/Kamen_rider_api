8ce9e07c83039186141dd7ec78220be28d95fe56
SebaBurst <sebarem723@gmail.com>
2024-02-08 21:37:35 -0300

    update cors

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import { PORT } from './config.js';
 import cors from 'cors'; //import cors
 
 
+const app = express(); //create an instance of express
 app.use(cors()); //use the cors middleware
 
-const app = express(); //create an instance of express
 app.use(express.json()); //use the json middleware
 app.use('/api/', kamen_routes); //use the routes
 app.listen(PORT); //listen on port 3000 
