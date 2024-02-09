4a6c4ea18be9a5875732937e097f1d3ef2189e1f
SebaBurst <sebarem723@gmail.com>
2024-02-08 21:32:38 -0300

    add cors middware

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express'; //import express
 import kamen_routes from './routes/kamen_rider.routes.js'; //import the routes
 import { PORT } from './config.js';
+import cors from 'cors'; //import cors
+
+
+app.use(cors()); //use the cors middleware
+
 const app = express(); //create an instance of express
 app.use(express.json()); //use the json middleware
 app.use('/api/', kamen_routes); //use the routes
