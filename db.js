f1771e365da54eea2e0054a55ade92c6abaf89fe
Sebastián Retamales <sebarem723@gmail.com>
2024-07-09 23:00:22 -0400

    Update to PostgreSQL

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
-import { createPool } from 'mysql2/promise'; // Importing the mysql module
-import { PORT, DB_DATABASE, DB_HOST,DB_PASSWORD, DB_PORT, DB_USER } from './config.js';
+import pkg from "pg";
+const { Pool } = pkg;
 // Create a connection to the database
-export const pool = createPool({ // createPool is a method of mysql module
-    host: DB_HOST, // The host name of the database
-    user: DB_USER,    // The user name of the database
-    password: DB_PASSWORD,     // The password of the database
-    port: DB_PORT, // The port number of the database
-    database: DB_DATABASE, // The name of the database
-});
+export const pool =  new Pool({
+    connectionString: process.env.POSTGRES_URL,
+  })
 
